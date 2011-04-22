2975d605d22f9750c16a6cc9b4091f8860b1536b
L. Costa <lcosta.job@gmail.com>
2011-04-22 22:35:43 +0100

    client.h

---
+++ client.h
@@ -0,0 +1,36 @@
+#ifndef _client_h_
+#define _client_h_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Client {
+protected:
+  int id;
+  string name;
+  int age;
+  
+public:
+  Client();	
+  ~Client();
+
+  // START: gets and sets
+  int getId() const;
+  void setId(int value);
+  string getName() const;
+  void setName(string value);
+  int getAge() const;
+  void setAge(int value);
+  // END: gets and sets  
+  
+  
+  // START: methods
+  // END: methods
+  
+
+};
+
+#endif
