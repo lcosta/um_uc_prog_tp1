b7e8ebeca3b5cf941ac8235196afd60531d20084
L. Costa <lcosta.job@gmail.com>
2011-04-25 16:06:16 +0100

    finish

--- testes/typeid_polymorphic_class_hierarchy.cpp
+++ testes/typeid_polymorphic_class_hierarchy.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 #include <typeinfo>
-//#include <fstream.h>
+#include <fstream.h>
 using namespace std;
 class Mammal {
 public:
@@ -15,23 +15,26 @@ public:
   }
   
   void write(std::ostream &out){
-    out << name << endl;
+    // propriedades comuns
+    out << name;
     
   }
 };
 class Cat: public Mammal {
 public:
   string eye;
-  Cat(string const n){
+  Cat(string const n, string e){
     name = n;
-    eye = "blue";
+    eye = e;
     cout << "Init Class Cat with: " << name << "\n";
     
   }
   
   void write(std::ostream &out){
+    out << "cat\n";
     Mammal::write(out);
-    out << eye << endl;
+    out << "\n";
+    out << eye << "\n";
   }
   
 };
@@ -45,11 +48,13 @@ public:
   bool laysEggs() { 
     return true; 
   }
-  /*
+  
   void write(std::ostream &out){
+    out << "platypus\n";
     Mammal::write(out);
+    out << "\n";
   }
-  */
+  
 };
 
 
@@ -68,7 +73,7 @@ public:
   }
   
   void addCat(string const &n){
-    Cat *c = new Cat(n);
+    Cat *c = new Cat(n, "red");
     db[count] = c;
     count++;
     
@@ -98,52 +103,120 @@ public:
     cout << "\nData List\n";
     for (int i = 0; i < count; i++) {
       cout << "Print: " << ((Cat*)db[i])->name << "\n";
-      cout << typeid(*db[i]).name() << endl;
+      //cout << typeid(*db[i]).name() << endl;
     }
   }
   
+  void write(ostream &out){
+    cout << "Start Write Data:\n";
+    out << count << "\n";
+    for (int i = 0; i < count; i++) {
+      if ( typeid(*db[i]) == typeid(Cat) ) {
+        ((Cat*)db[i])->write(out);
+      }
+      else if ( typeid(*db[i]) == typeid(Platypus) ) {
+        ((Platypus*)db[i])->write(out);
+      }
+      
+    }
+    
+    cout << "End Write Data:\n";
+  }
   
+  
+  void saveToFile(){
+   	std::fstream outToFile;
+    outToFile.open("data.txt", ofstream::out|ofstream::trunc);
+    if(!outToFile.fail()) {
+      write(outToFile);
+      outToFile.close();
+    } 
+    else {
+      cout << "Error writing to file..." << endl;
+    } 
+  }
+  
+  
+  void readFromFile(){
+   
+    fstream inFromFile;
+    inFromFile.open("data.txt", ios::in);
+    if(!inFromFile.fail()) {
+      
+      int count_in_file;
+      inFromFile >> count_in_file; // lê o número de veículos no ficheiro
+      inFromFile.ignore(); // para saltar por cima do caractere de mudança de linha ('\n')
+      
+      for (int i= 0; i < count_in_file; i++) {
+        string class_name;
+        getline(inFromFile, class_name);
+
+       
+        string name;
+        getline(inFromFile, name);
+        if ( class_name.compare("cat") == 0 ) {
+          string eye;
+          getline(inFromFile, eye);
+          add(new Cat(name, eye));
+          
+        }
+        else if ( class_name.compare("platypus") == 0 ) {
+          add(new Platypus(name));
+        }
+        
+        
+      }
+      
+      /*
+      // cria um array para veículo com o dobro da dimensão do nº actual
+      currentSize = n_vehicles * 2; 
+      vehicles = new Vehicle*[currentSize];
+      
+      //lê o estado de cada um dos veículos
+      for(int i = 0; i < n_vehicles; i++) {
+        std::string s;
+        getline(inFromFile, s);
+        if(s.compare(typeid(Vehicle).name()) == 0) { // verifica qual o tipo de objecto
+          vehicles[i] = Vehicle::read(inFromFile);
+        }
+        else {
+          vehicles[i] = MotorVehicle::read(inFromFile);
+        }
+        inFromFile.ignore(); // para saltar por cima do caractere de mudança de linha ('\n')
+      }
+      
+      */
+      
+      //
+      //readFrom(inFromFile);
+      
+      
+      inFromFile.close();
+    }
+    else {
+      cout << "Error reading file..." << endl;
+      // inicializar o array de veículos
+      //currentSize = 2;
+      //vehicles = new Vehicle*[currentSize];
+      //n_vehicles = 0;
+    }
+    
+  }
   
 };
 
-Mammal ** p;
 
 
-void  create(){
-  
-  /*
-   Mammal AnyMammal;
-   Cat cat;
-   Platypus platypus;
-   */
-  
-  p[0] = new Mammal;
-  p[1] = new Cat("Field");
-  p[2] = new Platypus("xx");
-  
-  ///cout << p[1]->name;
-  
-  //cout << ((Mammal*)p[1])->name;
-  
-  //cout << (*p[1])->name;
-  
-  /*
-   p[0] = &AnyMammal;
-   p[1] = &cat;
-   p[2] = &platypus;
-   */
-}
 
-void list(){
-  cout << "p is pointing to an object of type ";
-  cout << typeid(*p[0]).name() << endl;
+
+  //Mammal ** p;
+  //p[0] = new Mammal;
+  //p[1] = new Cat("Field");
+  //p[2] = new Platypus("xx");
   
-  cout << "p is pointing to an object of type ";
-  cout << typeid(*p[1]).name() << endl;
-  
-  cout << "p is pointing to an object of type ";
-  cout << typeid(*p[2]).name() << endl;
-}
+  //cout << "p is pointing to an object of type ";
+  //cout << typeid(*p[0]).name() << endl;
+
 
 
 
@@ -154,38 +227,24 @@ int main()
 {
 
   
-  //p = new Mammal*[10];
-  //create();
-  //list();
+  d.readFromFile();
+  d.list();
   
+  /*
   
-  
-  //Data d = Data();
-  //d = new Data;
-  
-  
-  
-  d.addCat("gato 1");
-  d.addCat("gato 2");
-  d.addCat("gato 3");
+  //d.addCat("gato 1");
+  //d.addCat("gato 2");
+  //d.addCat("gato 3");
 
   Cat *c3;
-  c3 = new Cat("Field");
+  c3 = new Cat("Field", "blue");
   d.add(c3);
   
-  d.add(new Cat("Field1000"));
+  d.add(new Cat("Field1000", "green"));
   d.add(new Platypus("platypus 1"));
   
   
   d.list();
-  
-  //write_to_binary_file(d);
-  
-  /*
-   Cat *c;
-   c = new Cat;
-   d.add(&c);
-   */
   
 
   c3->name = "x1";
@@ -199,6 +258,13 @@ int main()
   cout << c3->getName() << "\n";
   cout << c3->name << "\n";
   
+  
+  //d.write(cout);
+  
+  d.saveToFile();
+   
+   
+   */
   
   cout << "END\n";
   
