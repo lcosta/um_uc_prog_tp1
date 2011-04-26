b3fd06ac35ec6543bd5cb6e32ae9a5573ea8f8ed
L. Costa <lcosta.job@gmail.com>
2011-04-26 05:22:26 +0100

    mais pronto.... quase... nunca mais...

--- app.cpp
+++ app.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <typeinfo>
 #include <string>
+#include <dirent.h>
 
 #include "drugs_package.h"
 
@@ -21,98 +22,69 @@ Pharmacy current_pharmacy = Pharmacy();
 
 void printMenuMain();
 void printMenuOptions();
+void getdir (string dir);
 
 
 int main() {
   
-  
-  
-  current_pharmacy.setId("100");
-  
-  
-
-  current_pharmacy.readFromFile();
-  
-  //current_pharmacy.addClient(new Client(1,"mosca",27));
-  //current_pharmacy.addClient(new Client(1,"sapo",25));
-  
-  current_pharmacy.listClients();
-  
-  /*
-  current_pharmacy.addDrug(new Sachet(1,
-                                      "Sachet 1",
-                                      "lab 1",
-                                      Date(1,1,1),
-                                      10.50,
-                                      0.15,
-                                      10,
-                                      201,
-                                      6));
-  
-  current_pharmacy.addDrug(new Syrup(1,
-                                     "Syrup 1",
-                                     "lab 1",
-                                     Date(1,1,1),
-                                     15.50,
-                                     0.65,
-                                     10,
-                                     202,
-                                     5,
-                                     "vidro"));
-  
-  current_pharmacy.addDrug(new Tablet(1,
-                                      "Tablet 1",
-                                      "lab 1",
-                                      Date(1,1,1),
-                                      25.50,
-                                      0.25,
-                                      10,
-                                      203));
-  
-  current_pharmacy.addDrug(new Varied(1,
-                                  "Varied 1",
-                                  "lab 1",
-                                  Date(1,1,1),
-                                  35.50,
-                                  0.50,
-                                  10,
-                                  204));
-  
-  */
-  
-  current_pharmacy.listDrugs();
-  
-  current_pharmacy.setName("farma 1");
-  
-  //current_pharmacy.saveToFile();
-  
-
-  
   bool run = true;
   int opt;
   int opt_phar;
+  string name_in;
   
+  int go_to = -1;
   while(run) {
-    printMenuMain();
-    cin >> opt_phar;
+    
+    
+    if(go_to != -1){
+      opt_phar = go_to;
+      go_to = -1;
+    }
+    else {
+      printMenuMain();
+      cin >> opt_phar;
+      cin.ignore();
+    }
+
+    
     
     switch (opt_phar) {
         
       case 1: // Criar Farmacia
         break;
         
-      case 2: // Editar Farmacia
+      case 2: // Abrir Farmacia
+        cout << "Escolha a Farmacia (sem .txt):\n";
+        getdir("./db");
+        cin >> opt_phar;
+        current_pharmacy.readFromFile(opt_phar);
+        current_pharmacy.setId(opt_phar);
+        cout << "A farmacia '" << current_pharmacy.getName() << "' foi carregada.\n";
+        go_to = 3;
         break;
+    
         
-      case 3:
-        break; // Listar Farmacia
-        
-      case 4: // Menu opcoes
+      case 3: // Menu opcoes
         printMenuOptions();
-        cin >> opt;
         
+        cin >> opt;
+        cin.ignore();
+        go_to = 3;
         switch (opt) {
             
+          case 0:// Editar Farmacia
+            
+            cout << "Introduza o novo nome? ";
+            //cin >> name_in;
+            //cin.getline (name_in, 100);
+            getline (cin,name_in);
+            current_pharmacy.setName(name_in);
+            
+            current_pharmacy.saveToFile();
+            
+            cout << "O nome da farmacia foi alterado para '" << current_pharmacy.getName() << "'.\n";
+            
+            break;
             
           case 1:// Inserir medicamento
             break;
@@ -121,9 +93,11 @@ int main() {
             break;
             
           case 3:// Listar medicamneto
+            current_pharmacy.listDrugs();
             break;
             
           case 4:// Listar cliente
+            current_pharmacy.listClients();
             break;
             
           case 5:// Editar medicamento
@@ -136,6 +110,7 @@ int main() {
             break;
             
           case 8:// Menu principal 
+            go_to = -1;
             break;
             
             
@@ -149,7 +124,7 @@ int main() {
         
         break;
         
-      case 5:// Sair 
+      case 4:// Sair 
         run = false;
         break;
       default:cout << "Tente novamente..." << endl;
@@ -167,16 +142,16 @@ void printMenuMain() {
   std::cout << " **** Menu Principal ****" << std::endl;
   std::cout << "|----------------------|" << std::endl;
   std::cout << "|  1 - Criar Farmacia  |" << std::endl;
-  std::cout << "|  2 - Editar Farmacia |" << std::endl;
-  std::cout << "|  3 - Listar Farmacia |" << std::endl;
-  std::cout << "|  4 - Menu Opcoes     |" << std::endl;
-  std::cout << "|  5 - Sair            |" << std::endl;
+  std::cout << "|  2 - Abrir Farmacia  |" << std::endl;
+  std::cout << "|  3 - Menu Opcoes     |" << std::endl;
+  std::cout << "|  4 - Sair            |" << std::endl;
   std::cout << " ---------------------- " << std::endl;
 }
 
 void printMenuOptions() {
   std::cout << " ******* Menu Opcoes  ********" << std::endl;
   std::cout << "|****************************|" << std::endl;
+  std::cout << "|   0 - Editar farmacia      |" << std::endl;
   std::cout << "|   1 - Inserir medicamento  |" << std::endl;
   std::cout << "|   2 - Inserir cliente      |" << std::endl;
   std::cout << "|   3 - Listar medicamento(s)|" << std::endl;
@@ -187,4 +162,20 @@ void printMenuOptions() {
   std::cout << "|   8 - Menu Principal       |" << std::endl;
   std::cout << "|   9 - Sair                 |" << std::endl;
   std::cout << " **************************** " << std::endl;
+}
+
+void getdir (string dir)
+{
+  DIR *dp;
+  struct dirent *dirp;
+  if((dp  = opendir(dir.c_str())) == NULL) {
+    cout << "Error - opening " << dir << endl;
+  }
+  
+  while ((dirp = readdir(dp)) != NULL) {
+    if(strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0){
+      cout << string(dirp->d_name) << "\n";
+    }
+  }
+  closedir(dp);
 }
